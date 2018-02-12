84d86c108995d0b0ac4e3645dcc4586708955389
Jebbss <jebsumer@yahoo.com>
2018-02-12 13:42:47 -0600

    1.0

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+# Github flavored markdown previewer
+This project was made for freeCodeCamps data visualization certificate
+
+[Learn more here](https://www.freecodecamp.org/challenges/build-a-markdown-previewer)
+
+# Demo
+Requires [npm](https://www.npmjs.com/get-npm)
+```
+git clone https://github.com/Jebbss/markdown_previewer.git
+cd markdown_previewer
+npm install
+npm start
+```
+
+Made with
+- HTML/CSS
+- React
+- [marked](https://www.npmjs.com/package/marked)
--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import ControlLabel from 'react-bootstrap/lib/ControlLabel';
+import FormControl from 'react-bootstrap/lib/FormControl';
+
+let marked = require("marked");
 
 class App extends Component {
+
+  state = {
+    markdown: ""
+  }
+
+  updateMarkdown = function(markdown) {
+    this.setState({markdown})
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container">
+        <div>
+          <FormGroup controlId="FormControlsTextArea">
+            <ControlLabel>Markdown Input</ControlLabel>
+            <FormControl componentClass="textarea" placeholder="Enter Markdown"
+            value = {this.state.markdown} onChange = {(event)=>this.updateMarkdown(event.target.value)}
+            ></FormControl>
+          </FormGroup>
+        </div>
+        <div>
+          <h1>Markdown Output</h1>
+          <div className = "border border-dark" dangerouslySetInnerHTML = {{__html: marked(this.state.markdown)}}>
+          </div>
+        </div>
       </div>
     );
   }
