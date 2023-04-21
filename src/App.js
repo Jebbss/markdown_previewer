57b0170e2c1d089bc7c8b2bfc1b908f93c434b01
jeb <jebsumerac@gmail.com>
2023-04-21 16:25:55 -0500

    react upgrade

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Form from 'react-bootstrap/Form';
-import { marked } from 'marked';
+import {marked} from 'marked';
 
 class App extends Component {
 
-  state = {
-    markdown: ""
-  }
+    state = {
+        markdown: ""
+    }
 
-  updateMarkdown = function(markdown) {
-    this.setState({markdown})
-  }
+    updateMarkdown = function (markdown) {
+        this.setState({markdown})
+    }
 
-  render() {
-    return (
-      <div className="App container">
-        <div>
-          <Form.Group controlId="FormControlsTextArea">
-            <Form.Label>Markdown Input</Form.Label>
-            <Form.Control componentClass="textarea" placeholder="Enter Markdown"
-            value = {this.state.markdown} onChange = {(event)=>this.updateMarkdown(event.target.value)}
-            ></Form.Control>
-          </Form.Group>
-        </div>
-        <div>
-          <h1>Markdown Output</h1>
-          <div className = "border border-dark output" dangerouslySetInnerHTML = {{__html: marked.parse(this.state.markdown)}}>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App container h-100">
+                <div className="row row-cols-1 row-cols-sm-2 h-100">
+                    <div className="col h-50">
+                        <Form.Group controlId="FormControlsTextArea" className="h-75">
+                            <h1>Markdown Input</h1>
+                            <Form.Control as="textarea" placeholder="Enter Markdown" className="h-100 border border-dark"
+                                          value={this.state.markdown}
+                                          onChange={(event) => this.updateMarkdown(event.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+                    </div>
+                    <div className="col h-50">
+                        <h1>Markdown Output</h1>
+                        <div className="h-75 border border-dark output"
+                             dangerouslySetInnerHTML={{__html: marked.parse(this.state.markdown)}}>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
 registerServiceWorker();
