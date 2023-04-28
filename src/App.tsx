7c67a2c96cee46a9c2dadb9e14fa597b66b59b4b
jeb <jebsumerac@gmail.com>
2023-04-28 12:40:57 -0500

    bespoke css

--- src/App.tsx
+++ src/App.tsx
@@ -16,25 +16,25 @@ class App extends Component {
     render() {
         return (
             <div className="page-div">
-                <div className="markdown-divs">
-                    <div className="input-div">
+                <div className="row-95h">
+                    <div className="input-div half-screen">
                         <div className="input-form-group">
-                            <h1>Markdown Input</h1>
-                            <textarea placeholder="Enter Markdown" className="input-text-area"
+                            <h1 className="heading">Markdown Input</h1>
+                            <textarea placeholder="Enter Markdown" className="input-text-area text-area"
                                           value={this.state.markdown}
                                           onChange={(event) => this.updateMarkdown(event.target.value)}
                             ></textarea>
                         </div>
                     </div>
-                    <div className="output-div">
+                    <div className="output-div half-screen">
                         <h1>Markdown Output</h1>
-                        <div className="output-text-area"
+                        <div className="output-text-area text-area"
                              dangerouslySetInnerHTML={{__html: marked.parse(this.state.markdown)}}>
                         </div>
                     </div>
                 </div>
                 <div className="syntax-link-div">
-                    <a href="https://daringfireball.net/projects/markdown/syntax">Syntax</a>
+                    <a className="syntax-link" href="https://daringfireball.net/projects/markdown/syntax" target="_blank">Syntax Guide</a>
                 </div>
             </div>
         );
